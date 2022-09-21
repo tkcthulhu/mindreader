8ca5a65dd9028544c2c123b40269d05c0e49bd90
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-09-21 16:07:48 +0000

    figuring out how to increment

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ const headerText = [
     '&'
 ]
 
-const blueButtonText = [
-    null,
-    'NEXT',
+const //blueButtonText = [
+    nu//ll,
+    'N//EXT',
     'REVEAL'
 ]
 
@@ -30,63 +30,82 @@ const helperText = [
 ]
 */
 
-const resetImage = '../img/reset.png'
-
-const blueButton = document.getElementById('blue-button');
-
-let createBlueButton = document.createElement('button');
-createBlueButton.innerText = 'NEXT';
-//btn.addEventListener('click', )
-blueButton.appendChild(createBlueButton);
-
-let state = {
+//let state = {
 
 
     
-    pages : [
+    let pages = [
         {
             headerText: 'I can read your mind',
-            blueButtonText: null,
+            //blueButtonText: null,
             exampleText: null,
             helperText: null,
             //goResetButton: 'GO',
         },
         {
             headerText: 'Pick a number from 01-99',
-            blueButtonText: null,
-            exampleText: null,
-            helperText: null,
+            //blueButtonText: null,
+            exampleText: 'when you have your number click next',
+            helperText: 'click next to proceed',
             //goResetButton: resetImage,
         },
         {
             headerText: 'Add both digits together to get a new number',
-            blueButtonText: null,
-            exampleText: null,
-            helperText: null,
+            //blueButtonText: null,
+            exampleText: 'Ex: 14 is 1 + 4 = 5',
+            helperText: 'click next to proceed',
             //goResetButton: resetImage,
         },
         {
             headerText: 'Subract your new number from the original number',
-            blueButtonText: null,
-            exampleText: null,
-            helperText: null,
+            //blueButtonText: null,
+            exampleText: 'Ex: 14 - 5 = 9',
+            helperText: 'click next to proceed',
             //goResetButton: resetImage,
         },
         {
             headerText: null, //[array of numbers and symbols and shit]
-            blueButtonText: null,
-            exampleText: null,
-            helperText: null,
+            //blueButtonText: null,
+            exampleText: 'Find your new number',
+            helperText: 'Note the symbol beside the number',
             //goResetButton: resetImage,
         },
         {
             headerText: '&',
-            blueButtonText: null,
-            exampleText: null,
-            helperText: null,
+            //blueButtonText: null,
+            exampleText: 'Your symbol is:',
+            helperText: '&',
             //goResetButton: resetImage,
         }
     ]
-}
+//}
+
+const resetImage = '../img/reset.png'
+
+const header = document.getElementById('header-text');
+const blueButton = document.getElementById('blue-button');
+const example = document.getElementById('example-text');
+const helper = document.getElementById('helper-text');
+const goResetButton = document.getElementById('GO/Reset-button')
+
+let createBlueButton = document.createElement('button');
+createBlueButton.innerText = 'NEXT';
+let pg = 0;
+
+createBlueButton.addEventListener('click', function incrementPage() {
+    let pg = 0; pg <= 6; pg++;
+    console.log(pg)
+    //return pg += 1;
+})
+blueButton.appendChild(createBlueButton);
 
 createBlueButton;
+
+let headerText = pages[incrementPage()].headerText;
+header.textContent = headerText;
+
+let exampleText = pages[pg].exampleText;
+example.textContent = exampleText;
+
+let helperText = pages[pg].helperText;
+helper.textContent = helperText;
