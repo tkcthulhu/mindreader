84edd4ef34322bd806e6ce75e83109bf8af8d3d4
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-09-21 15:17:24 +0000

    blue button now shows up

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
+/*
 const headerText = [
     'I can read your mind',
     'Pick a number from 01-99',
     'Add both digits together to get a new number',
     'Subract your new number from the original number',
-    //[array of numbers and symbols and shit]
+    [array of numbers and symbols and shit]
     '&'
 ]
 
-const blueButton = [
-    //hidden,
+const blueButtonText = [
+    null,
     'NEXT',
     'REVEAL'
 ]
@@ -19,12 +20,73 @@ const exampleText = [
     'Ex: 14 - 5 = 9',
     'Find your new number',
     'Your symbol is:'
-
 ]
 
 const helperText = [
-    //hidden,    
+    null,    
     'click next to proceed',
     'Note the symbol beside the number',
     '&'
 ]
+*/
+
+const resetImage = '../img/reset.png'
+
+const blueButton = document.getElementById('blue-button');
+
+let createBlueButton = document.createElement('button');
+createBlueButton.innerText = 'NEXT';
+//btn.addEventListener('click', )
+blueButton.appendChild(createBlueButton);
+
+let state = {
+
+
+    
+    pages : [
+        {
+            headerText: 'I can read your mind',
+            blueButtonText: null,
+            exampleText: null,
+            helperText: null,
+            //goResetButton: 'GO',
+        },
+        {
+            headerText: 'Pick a number from 01-99',
+            blueButtonText: null,
+            exampleText: null,
+            helperText: null,
+            //goResetButton: resetImage,
+        },
+        {
+            headerText: 'Add both digits together to get a new number',
+            blueButtonText: null,
+            exampleText: null,
+            helperText: null,
+            //goResetButton: resetImage,
+        },
+        {
+            headerText: 'Subract your new number from the original number',
+            blueButtonText: null,
+            exampleText: null,
+            helperText: null,
+            //goResetButton: resetImage,
+        },
+        {
+            headerText: null, //[array of numbers and symbols and shit]
+            blueButtonText: null,
+            exampleText: null,
+            helperText: null,
+            //goResetButton: resetImage,
+        },
+        {
+            headerText: '&',
+            blueButtonText: null,
+            exampleText: null,
+            helperText: null,
+            //goResetButton: resetImage,
+        }
+    ]
+}
+
+createBlueButton;
